508b2a4ad178ea9a79d49b6293b95511328518cf
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-05-23 17:24:03 +0100

    build(frontend): convert to ESM
    - add `type: module`.
    - replace `require` and `module.exports` with `import` and `export default` in config files.

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,11 +1,11 @@
-const js = require('@eslint/js');
-const prettier = require('eslint-config-prettier');
-const reactRecommended = require('eslint-plugin-react/configs/recommended');
-const reactJSXRuntime = require('eslint-plugin-react/configs/jsx-runtime');
-const reactHooks = require('eslint-plugin-react-hooks');
-const typescript = require('typescript-eslint').default;
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import reactRecommended from 'eslint-plugin-react/configs/recommended.js';
+import reactJSXRuntime from 'eslint-plugin-react/configs/jsx-runtime.js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import typescript from 'typescript-eslint';
 
-module.exports = [
+export default [
   js.configs.recommended,
   reactRecommended,
   reactJSXRuntime,
