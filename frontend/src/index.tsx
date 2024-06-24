2850a4fb453a0339f2f92b5e00b1610afd98887b
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-06-24 22:33:14 +0100

    render the app with createRoot().render()

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,6 @@
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
 const container = document.getElementById('root');
-render(<App />, container);
+const root = createRoot(container);
+root.render(<App />);
