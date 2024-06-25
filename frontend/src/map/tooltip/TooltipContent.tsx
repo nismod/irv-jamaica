f9ed924c6052ba32662994be2bfd72fa7bff901c
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-06-25 18:18:33 +0100

    refactor(frontend): generalise TooltipContent
    Generalise TooltipContent with a `useTooltipLayers` hook, which supplies a map of layer IDs to their respective tooltip components and layer state. This should make it easier to add new layers, by extending the map.

--- frontend/src/map/tooltip/TooltipContent.tsx
+++ frontend/src/map/tooltip/TooltipContent.tsx
@@ -6,11 +6,21 @@ import { VectorHoverDescription } from './content/VectorHoverDescription';
 import { RasterHoverDescription } from './content/RasterHoverDescription';
 import { RegionHoverDescription } from './content/RegionHoverDescription';
 import { hasHover, hoverState } from 'lib/data-map/interactions/interaction-state';
-import { InteractionTarget } from 'lib/data-map/interactions/use-interactions';
+import {
+  InteractionTarget,
+  VectorTarget,
+  RasterTarget,
+} from 'lib/data-map/interactions/use-interactions';
 import { showPopulationState } from 'state/regions';
 import { SolutionHoverDescription } from './content/SolutionHoverDescription';
 import { DroughtHoverDescription } from './content/DroughtHoverDescription';
 import { ErrorBoundary } from 'lib/react/ErrorBoundary';
+
+type MapDataLayer = InteractionTarget<VectorTarget | RasterTarget>;
+type TooltipLayer = {
+  component: FC<{ hoveredObject: MapDataLayer }>;
+  target: MapDataLayer | MapDataLayer[] | null;
+};
 
 const TooltipSection = ({ children }) => (
   <Box p={1} borderBottom="1px solid #ccc">
@@ -18,26 +28,64 @@ const TooltipSection = ({ children }) => (
   </Box>
 );
 
-export const TooltipContent: FC = () => {
-  const hoveredVector = useRecoilValue(hoverState('assets')) as InteractionTarget<any>;
-  const hoveredRasters = useRecoilValue(hoverState('hazards')) as InteractionTarget<any>[];
-  const hoveredRegion = useRecoilValue(hoverState('regions')) as InteractionTarget<any>;
-  const hoveredSolution = useRecoilValue(hoverState('solutions')) as InteractionTarget<any>;
-  const hoveredDrought = useRecoilValue(hoverState('drought')) as InteractionTarget<any>;
+const useLayerTarget = (layerId: string) => useRecoilValue(hoverState(layerId));
+/**
+ * Define tooltip properties for data layers.
+ * @returns a map of tooltip components and their respective data layers, mapped by layer ID.
+ */
+function useTooltipLayers(): Map<string, TooltipLayer> {
+  return new Map<string, TooltipLayer>([
+    [
+      'assets',
+      {
+        component: VectorHoverDescription,
+        target: useLayerTarget('assets'),
+      },
+    ],
+    [
+      'hazards',
+      {
+        component: RasterHoverDescription,
+        target: useLayerTarget('hazards'),
+      },
+    ],
+    [
+      'regions',
+      {
+        component: RegionHoverDescription,
+        target: useLayerTarget('regions'),
+      },
+    ],
+    [
+      'solutions',
+      {
+        component: SolutionHoverDescription,
+        target: useLayerTarget('solutions'),
+      },
+    ],
+    [
+      'drought',
+      {
+        component: DroughtHoverDescription,
+        target: useLayerTarget('drought'),
+      },
+    ],
+  ]);
+}
 
+export const TooltipContent: FC = () => {
+  const layers = useTooltipLayers();
+  const layerEntries = [...layers.entries()];
   const regionDataShown = useRecoilValue(showPopulationState);
 
-  const assetsHovered = hasHover(hoveredVector);
-  const hazardsHovered = hasHover(hoveredRasters);
-  const regionsHovered = hasHover(hoveredRegion);
-  const solutionsHovered = hasHover(hoveredSolution);
-  const droughtHovered = hasHover(hoveredDrought);
-  const doShow =
-    assetsHovered ||
-    hazardsHovered ||
-    (regionDataShown && regionsHovered) ||
-    solutionsHovered ||
-    droughtHovered;
+  function hasHovered([type, { target }]) {
+    if (type === 'regions' && !regionDataShown) {
+      return false;
+    }
+    return hasHover(target);
+  }
+
+  const doShow = layerEntries.some(hasHovered);
 
   if (!doShow) return null;
 
@@ -45,36 +93,26 @@ export const TooltipContent: FC = () => {
     <Paper>
       <Box minWidth={200}>
         <ErrorBoundary message="There was a problem displaying the tooltip.">
-          {solutionsHovered && (
-            <TooltipSection>
-              <SolutionHoverDescription hoveredObject={hoveredSolution} />
-            </TooltipSection>
-          )}
-          {assetsHovered ? (
-            <TooltipSection>
-              <VectorHoverDescription hoveredObject={hoveredVector} />
-            </TooltipSection>
-          ) : null}
-          {droughtHovered ? (
-            <TooltipSection>
-              <DroughtHoverDescription hoveredObject={hoveredDrought} />
-            </TooltipSection>
-          ) : null}
-          {hazardsHovered ? (
-            <TooltipSection>
-              {hoveredRasters.map((hr) => (
-                <RasterHoverDescription
-                  hoveredObject={hr}
-                  key={`${hr.viewLayer.id}-${hr.target.id}`}
-                />
-              ))}
-            </TooltipSection>
-          ) : null}
-          {regionsHovered ? (
-            <TooltipSection>
-              <RegionHoverDescription hoveredObject={hoveredRegion} />
-            </TooltipSection>
-          ) : null}
+          {layerEntries.map((layer) => {
+            const [type, { component: Component, target }] = layer;
+            if (hasHovered(layer)) {
+              if (Array.isArray(target)) {
+                return (
+                  <TooltipSection key={type}>
+                    {target.map((hr) => (
+                      <Component hoveredObject={hr} key={hr.viewLayer.id} />
+                    ))}
+                  </TooltipSection>
+                );
+              }
+              return (
+                <TooltipSection key={type}>
+                  <Component hoveredObject={target} />
+                </TooltipSection>
+              );
+            }
+            return null;
+          })}
         </ErrorBoundary>
       </Box>
     </Paper>
