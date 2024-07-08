fe3d1f46ae258889f60dedaa4dcc2a76eb397423
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-07-08 17:04:33 +0100

    refactor(frontend): raster colour map config
    Configure raster colour maps as `config/hazards/color-maps`, for consistency with other layer configs.

--- frontend/src/config/view-layers/hazards/HazardHoverDescription.tsx
+++ frontend/src/config/view-layers/hazards/HazardHoverDescription.tsx
@@ -3,7 +3,8 @@ import { FC } from 'react';
 import { RasterHoverDescription } from 'lib/data-map/types';
 import { RasterHoverDescription as RasterTooltip } from 'map/tooltip/content/RasterHoverDescription';
 
-import { HAZARDS_METADATA, HAZARD_COLOR_MAPS } from './metadata';
+import * as HAZARD_COLOR_MAPS from './color-maps';
+import { HAZARDS_METADATA } from './metadata';
 
 export const HazardHoverDescription: FC<RasterHoverDescription> = ({ target, viewLayer }) => {
   const { label, dataUnit } = HAZARDS_METADATA[viewLayer.id];
---
+++ frontend/src/config/view-layers/hazards/color-maps.ts
@@ -0,0 +1,19 @@
+export const fluvial = {
+  scheme: 'blues',
+  range: [0, 10],
+};
+
+export const coastal = {
+  scheme: 'greens',
+  range: [0, 10],
+};
+
+export const surface = {
+  scheme: 'purples',
+  range: [0, 10],
+};
+
+export const cyclone = {
+  scheme: 'reds',
+  range: [0, 75],
+};
