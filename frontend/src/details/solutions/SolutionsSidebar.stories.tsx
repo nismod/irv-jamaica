965d62e7fd4402d57c93ede888105c46786598e3
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-08-13 21:43:52 +0100

    test(frontend): add details selection state to stories (#103)
    Add Recoil state to the Details sidebar stories, to simulate map feature selection in the app.

--- frontend/src/details/solutions/SolutionsSidebar.stories.tsx
+++ frontend/src/details/solutions/SolutionsSidebar.stories.tsx
@@ -1,12 +1,45 @@
 import { StoryObj, Meta } from '@storybook/react';
+import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
 
+import { selectionState } from 'lib/state/interactions/interaction-state';
 import mockTerrestrialFeature from 'mocks/details/solutions/mockTerrestrialFeature.json';
 import mockMarineFeature from 'mocks/details/solutions/mockMarineFeature.json';
-import { SolutionsSidebarContent } from './SolutionsSidebarContent';
+import { SolutionsSidebar } from './SolutionsSidebar';
+
+function fixedWidthDecorator(Story) {
+  return (
+    <div style={{ width: '300px' }}>
+      <Story />
+    </div>
+  );
+}
+
+function dataLoaderDecorator(Story, { args }) {
+  const [, setFeatureSelection] = useRecoilState(selectionState('solutions'));
+  const mockSelection = {
+    interactionGroup: 'solutions',
+    interactionStyle: 'vector',
+    target: {
+      feature: args.feature,
+    },
+    viewLayer: {
+      id: args.id,
+      group: null,
+      fn: () => {},
+    },
+  };
+  useEffect(() => {
+    setFeatureSelection(mockSelection);
+  }, []);
+
+  return <Story />;
+}
 
 const meta = {
-  title: 'Details/SolutionsSidebarContent',
-  component: SolutionsSidebarContent,
+  title: 'Details/SolutionsSidebar',
+  component: SolutionsSidebar,
+  decorators: [fixedWidthDecorator, dataLoaderDecorator],
 } as Meta;
 
 export default meta;
@@ -15,15 +48,13 @@ type Story = StoryObj<typeof meta>;
 export const Terrestrial: Story = {
   args: {
     feature: mockTerrestrialFeature,
-    solutionType: 'terrestrial',
-    showRiskSection: true,
+    id: 'terrestrial',
   },
 };
 
 export const Marine: Story = {
   args: {
     feature: mockMarineFeature,
-    solutionType: 'marine',
-    showRiskSection: true,
+    id: 'marine',
   },
 };
