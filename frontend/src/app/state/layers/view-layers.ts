78bd25a7f053e96aee6b3c8c76a42f1f6635e883
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-11-14 16:43:33 +0000

    refactor(frontend): view layer config (#193)
    `VIEW_LAYERS` is only used in one place, so move it into `app/state`.

--- frontend/src/app/state/layers/view-layers.ts
+++ frontend/src/app/state/layers/view-layers.ts
@@ -3,11 +3,22 @@ import { waitForAll, selector, selectorFamily } from 'recoil';
 import { ViewLayer } from 'lib/data-map/view-layers';
 import { ConfigTree } from 'lib/nested-config/config-tree';
 
-import { VIEW_LAYERS } from 'app/config/view-layers';
 import { importLayerState } from 'data-layers/state';
 
 import { featureBoundingBoxLayerState } from './ui/featureBoundingBox';
 import { labelsLayerState } from './ui/labels';
+
+const VIEW_LAYERS = [
+  'regions',
+  'droughtRisks',
+  'terrestrial',
+  'marine',
+  'hazards',
+  'risks',
+  'buildings',
+  'networks',
+  'droughtOptions',
+] as string[];
 
 const viewLayerConfig = selectorFamily<ViewLayer, string>({
   key: 'viewLayerConfig',
