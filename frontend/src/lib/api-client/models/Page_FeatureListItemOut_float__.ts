cbabb8483803ad337a29a81e45392cd012993c23
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-11-19 13:43:32 +0000

    refactor(frontend): run generate-client (#201)
    Run the `generate-client` script to make sure that the TypeScript client is up-to-date with the backend.

--- frontend/src/lib/api-client/models/Page_FeatureListItemOut_float__.ts
+++ frontend/src/lib/api-client/models/Page_FeatureListItemOut_float__.ts
@@ -7,6 +7,7 @@ import type { FeatureListItemOut_float_ } from './FeatureListItemOut_float_';
 export type Page_FeatureListItemOut_float__ = {
     items: Array<FeatureListItemOut_float_>;
     total: number;
-    page: number;
-    size: number;
+    page?: number;
+    size?: number;
+    pages?: number;
 };
