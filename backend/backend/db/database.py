22057d45ddc7ad2d75f0122682cb15878e7b1af0
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-12-02 15:22:55 +0000

    restore sessionmaker

--- backend/backend/db/database.py
+++ backend/backend/db/database.py
@@ -3,16 +3,17 @@ from typing import Annotated
 from fastapi import Depends
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 
 
 # pass empty connection string to use PG* environment variables
 # (see https://www.postgresql.org/docs/current/libpq-envars.html)
 engine = create_engine("postgresql+psycopg2://", future=True)
+Session = sessionmaker(engine, autoflush=False, autocommit=False)
 
 
 def get_session():
-    with Session(engine) as session:
+    with Session() as session:
         yield session
 
 
