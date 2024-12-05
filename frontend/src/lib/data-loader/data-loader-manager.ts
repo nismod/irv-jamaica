6400728006ea21b0107ba9883c8ab746feca6404
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-12-05 16:52:44 +0000

    refactor(frontend): data loader management (#224)
    - `DataLoaderManager`: split `getDataLoader` into `getDataLoader` and `registerDataLoader`.
    - `DataLoader`: add `hasData` property to check if the loader is empty.
    - `DataLoaderLayer`: check if a layer has data before triggering the default behaviour (load data for all feature IDs in a tile.)

--- frontend/src/lib/data-loader/data-loader-manager.ts
+++ frontend/src/lib/data-loader/data-loader-manager.ts
@@ -12,13 +12,18 @@ export class DataLoaderManager {
 
   public getDataLoader(layer: string, fieldSpec: FieldSpec) {
     const loaderKey = getLoaderKey(layer, fieldSpec);
-    let loader = this.loaders.get(loaderKey);
-    if (loader == null) {
+    if (!this.loaders.has(loaderKey)) {
       console.log(`Initialising data loader for ${layer} ${stringify(fieldSpec)}`);
-      loader = new DataLoader(this.nextLoaderId.toString(), layer, fieldSpec);
-      this.nextLoaderId += 1;
-      this.loaders.set(loaderKey, loader);
+      return this.registerDataLoader(layer, fieldSpec);
     }
+    return this.loaders.get(loaderKey);
+  }
+
+  public registerDataLoader(layer: string, fieldSpec: FieldSpec) {
+    const loader = new DataLoader(this.nextLoaderId.toString(), layer, fieldSpec);
+    this.nextLoaderId += 1;
+    const loaderKey = getLoaderKey(layer, fieldSpec);
+    this.loaders.set(loaderKey, loader);
     return loader;
   }
 
