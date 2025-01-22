e69616953269a25bab1bfc66c278361cff0f224f
Fred Thomas <fred.thomas@eci.ox.ac.uk>
2025-01-22 17:59:03 +0000

    Test metadata join

--- pixel_driller/tests/test_query_raster.py
+++ pixel_driller/tests/test_query_raster.py
@@ -1,35 +1,61 @@
 import unittest
 from pathlib import Path
 
+import numpy as np
+import pandas as pd
+
+import query
 from query import point_query, RasterStackMetadata
 
 
 class QueryRasterTestCase(unittest.TestCase):
     def test_query_raster(self):
+
+        # override METADATA for test
+        query.METADATA = pd.DataFrame(
+            {
+                "key": ["a", "b", "c", "d"],
+                "hazard": ["coastal", "coastal", "fluvial", "cyclone"],
+                "rp": [1, 2, 5, 10],
+                "rcp": ["2.6", "4.5", "4.5", "2.6"],
+                "epoch": [2050, 2050, 2050, 2100],
+                "confidence": [95.0, None, None, None],
+                "variable": ["depth", "depth", "depth", "speed"],
+                "unit": ["m", "m", "m", "m s-1"],
+            }
+        )
         datasets = [
             RasterStackMetadata(
                 "test", Path(__file__).parent / "fixtures" / "test.zarr", "EPSG:4326"
             )
         ]
         actual = point_query(datasets, 0.0, 0.0)
-        expected = [
-            {"key": "a", "band_data": 0.0},
-            {"key": "b", "band_data": 1.0},
-            {"key": "c", "band_data": 2.0},
-            {"key": "d", "band_data": 3.0},
-        ]
-        self.assertEqual(len(actual), 4)
-        self.assertEqual(actual, expected)
+        expected = {
+            "key": ["a", "b", "c", "d"],
+            "band_data": [0, 1, 2, 3],
+            "hazard": ["coastal", "coastal", "fluvial", "cyclone"],
+            "rp": [1, 2, 5, 10],
+            "rcp": ["2.6", "4.5", "4.5", "2.6"],
+            "epoch": [2050, 2050, 2050, 2100],
+            "confidence": [95.0, np.nan, np.nan, np.nan],
+            "variable": ["depth", "depth", "depth", "speed"],
+            "unit": ["m", "m", "m", "m s-1"],
+        }
+        self.assertEqual(sorted(actual), sorted(expected))
 
         actual = point_query(datasets, 0.1, 0.1)
-        expected = [
-            {"key": "a", "band_data": 12.0},
-            {"key": "b", "band_data": 13.0},
-            {"key": "c", "band_data": 14.0},
-            {"key": "d", "band_data": 15.0},
-        ]
-        self.assertEqual(len(actual), 4)
-        self.assertEqual(actual, expected)
+        expected = {
+            "key": ["a", "b", "c", "d"],
+            "band_data": [12, 13, 14, 15],
+            "hazard": ["coastal", "coastal", "fluvial", "cyclone"],
+            "rp": [1, 2, 5, 10],
+            "rcp": ["2.6", "4.5", "4.5", "2.6"],
+            "epoch": [2050, 2050, 2050, 2100],
+            "confidence": [95.0, np.nan, np.nan, np.nan],
+            "variable": ["depth", "depth", "depth", "speed"],
+            "unit": ["m", "m", "m", "m s-1"],
+        }
+        self.assertEqual(sorted(actual), sorted(expected))
 
 
 if __name__ == "__main__":
