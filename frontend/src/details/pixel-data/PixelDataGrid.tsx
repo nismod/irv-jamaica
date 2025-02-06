717aa387de3d99cb5f53327d69a09b74b17a7461
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2025-02-06 18:41:47 +0000

    feat: pixel driller frontend (#251)
    First pass at a frontend for the pixel-driller API. Clicking anywhere on the map opens a righthand sidebar panel with tables of hazard information for that point.

---
+++ frontend/src/details/pixel-data/PixelDataGrid.tsx
@@ -0,0 +1,52 @@
+import { DataGrid } from '@mui/x-data-grid';
+
+import {
+  pixelDrillerDataHeaders,
+  pixelDrillerDataRows,
+  pixelDrillerDataRPs,
+} from 'lib/state/pixel-driller';
+import { useRecoilValue } from 'recoil';
+
+import './hazard-table.css';
+import { Typography } from '@mui/material';
+
+const headings = {
+  cyclone: 'Cyclones',
+  fluvial: 'River flooding',
+  surface: 'Surface flooding',
+  coastal_mangrove: 'Coastal (mangrove)',
+  coastal_nomangrove: 'Coastal (no mangrove)',
+  coastal_nomangrove_minus_mangrove: 'Coastal (no mangrove - mangrove)',
+};
+
+const displayReturnPeriods = new Set([10, 20, 50, 100, 200, 500]);
+
+export const PixelDataGrid = ({ hazard }) => {
+  const headers = useRecoilValue(pixelDrillerDataHeaders);
+  const rows = useRecoilValue(pixelDrillerDataRows(hazard));
+  const dataReturnPeriods = useRecoilValue(pixelDrillerDataRPs(hazard));
+  const columns = [
+    { field: 'epoch', headerName: 'Epoch' },
+    { field: 'rcp', headerName: 'RCP' },
+  ];
+  const returnPeriods = displayReturnPeriods.intersection(dataReturnPeriods);
+
+  returnPeriods.forEach((rp) => {
+    columns.push({ field: `rp-${rp}`, headerName: `RP ${rp}` });
+  });
+  if (!headers.length) {
+    return null;
+  }
+  const variable = rows[0].variable;
+  const unit = rows[0].unit;
+
+  return (
+    <>
+      <Typography variant="subtitle2" component="h3">
+        {headings[hazard]}: {variable} ({unit})
+      </Typography>
+
+      <DataGrid columns={columns} rows={rows} density="compact" />
+    </>
+  );
+};
