nismod/irv-jamaica

commit b0e26a526cfe9fcecb110e83997149ad385e2eb5

build(deps): upgrade map packages (#266)
- upgrade `deck.gl`, `@loaders.gl`, `react-map-gl`, `maplibre-gl`, and `@turf/*` packages to latest.
- remove `deck.gl/typed`, now that deck.gl has full typescript support.
- replace `readPixelsToArray` in the interactions library.
- replace `feature.id` with `feature.properties.id` throughout the deck.gl layer code. Feature ID now seems to be stored on `feature.properties`.

frontend/src/lib/deck/layers/data-loader-layer.ts
@@ -1,5 +1,5 @@
 import { DataLoader } from 'lib/data-loader/data-loader';
-import { MapGeoJSONFeature } from 'maplibre-gl';
+import { getFeatureId } from '../utils/get-feature-id';
 
 export interface DataLoaderOptions {
   dataLoader: DataLoader;
@@ -13,7 +13,7 @@ export function dataLoaderLayer(tileProps, { dataLoader }: DataLoaderOptions) {
     tile: { content },
   } = tileProps;
   if (content && dataLoader) {
-    const ids: number[] = !dataLoader.hasData ? content.map((f: MapGeoJSONFeature) => f.id) : [];
+    const ids: number[] = !dataLoader.hasData ? content.map(getFeatureId) : [];
 
     dataLoader.loadDataForIds(ids);
   }

frontend/src/app/map/MapView.tsx
@@ -46,7 +46,6 @@ const AppNavigationControl = withProps(MapHudNavigationControl, {
 const AppScaleControl = withProps(MapHudScaleControl, {
   maxWidth: 100,
   unit: 'metric',
-  capturePointerMove: true,
 });
 
 const AppAttributionControl = withProps(MapHudAttributionControl, {

frontend/src/data-layers/droughtOptions/drought-options-view-layer.ts
@@ -12,6 +12,7 @@ import { VectorLegend } from 'lib/map/legend/VectorLegend';
 import { getDroughtDataAccessor } from './data-access';
 import { getDroughtOptionsDataFormats } from './data-formats';
 import { DroughtOptionsHoverDescription } from './DroughtOptionsHoverDescription';
+import { getFeatureId } from 'lib/deck/utils/get-feature-id';
 
 export function droughtOptionsViewLayer({ fieldSpec, colorSpec }): ViewLayer {
   const dataFn = getDroughtDataAccessor(fieldSpec);
@@ -30,7 +31,7 @@ export function droughtOptionsViewLayer({ fieldSpec, colorSpec }): ViewLayer {
     },
     fn: ({ deckProps, selection, zoom }) => {
       const target = selection?.target as VectorTarget;
-      const selectedFeatureIds = [target?.feature.id];
+      const selectedFeatureIds = [getFeatureId(target?.feature)];
       return selectableMvtLayer(
         {
           selectionOptions: {

frontend/src/data-layers/droughtRisks/drought-risk-view-layer.ts
@@ -12,6 +12,7 @@ import { VectorLegend } from 'lib/map/legend/VectorLegend';
 import { getDroughtDataAccessor } from './data-access';
 import { getDroughtRiskDataFormats } from './data-formats';
 import { DroughtRisksHoverDescription } from './DroughtRisksHoverDescription';
+import { getFeatureId } from 'lib/deck/utils/get-feature-id';
 
 export function droughtRiskViewLayer({ fieldSpec, colorSpec }): ViewLayer {
   const dataFn = getDroughtDataAccessor(fieldSpec);
@@ -31,7 +32,7 @@ export function droughtRiskViewLayer({ fieldSpec, colorSpec }): ViewLayer {
 
     fn: ({ deckProps, selection }) => {
       const target = selection?.target as VectorTarget;
-      const selectedFeatureIds = [target?.feature.id];
+      const selectedFeatureIds = [getFeatureId(target?.feature)];
       return selectableMvtLayer(
         {
           selectionOptions: {

frontend/src/data-layers/hazards/hazard-view-layer.ts
@@ -1,5 +1,4 @@
 import { createElement } from 'react';
-import GL from '@luma.gl/constants';
 import { HazardParams } from './domains';
 
 import { rasterTileLayer } from 'lib/deck/layers/raster-tile-layer';
@@ -34,7 +33,7 @@ export function getHazardId<
 }
 
 export function hazardViewLayer(hazardType: string, hazardParams: HazardParams): ViewLayer {
-  const magFilter = hazardType === 'cyclone' ? GL.NEAREST : GL.LINEAR;
+  const magFilter = hazardType === 'cyclone' ? 'nearest' : 'linear';
 
   const { returnPeriod, rcp, epoch, confidence } = hazardParams;
 
@@ -52,8 +51,7 @@ export function hazardViewLayer(hazardType: string, hazardParams: HazardParams):
       return rasterTileLayer(
         {
           textureParameters: {
-            [GL.TEXTURE_MAG_FILTER]: magFilter,
-            // [GL.TEXTURE_MAG_FILTER]: zoom < 12 ? GL.NEAREST : GL.NEAREST_MIPMAP_LINEAR,
+            magFilter,
           },
           opacity: hazardType === 'cyclone' ? 0.6 : 1,
         },

frontend/src/data-layers/marine/marine-view-layer.ts
@@ -9,6 +9,7 @@ import { fillColor } from 'lib/deck/props/style';
 import { VectorLegend } from 'lib/map/legend/VectorLegend';
 
 import { SolutionHoverDescription } from './MarineHoverDescription';
+import { getFeatureId } from 'lib/deck/utils/get-feature-id';
 
 function filterRange(value: boolean) {
   return value ? [1, 1] : [0, 1];
@@ -21,7 +22,7 @@ export function marineViewLayer({ dataFn, colorFn, filters }): ViewLayer {
     interactionGroup: 'solutions',
     fn: ({ deckProps, selection }) => {
       const target = selection?.target as VectorTarget;
-      const selectedFeatureIds = [target?.feature.id];
+      const selectedFeatureIds = [getFeatureId(target?.feature)];
       return selectableMvtLayer(
         {
           selectionOptions: {

frontend/src/data-layers/regions/population-view-layer.ts
@@ -13,6 +13,7 @@ import { VectorLegend } from 'lib/map/legend/VectorLegend';
 import { RegionLevel } from './metadata';
 import { REGIONS_SOURCE } from './source';
 import { RegionHoverDescription } from './RegionHoverDescription';
+import { getFeatureId } from 'lib/deck/utils/get-feature-id';
 
 export function populationViewLayer(regionLevel: RegionLevel): ViewLayer {
   const source = REGIONS_SOURCE;
@@ -43,7 +44,7 @@ export function populationViewLayer(regionLevel: RegionLevel): ViewLayer {
     }),
     fn: ({ deckProps, zoom, selection }) => {
       const target = selection?.target as VectorTarget;
-      const selectedFeatureIds = [target?.feature.id];
+      const selectedFeatureIds = [getFeatureId(target?.feature)];
       return selectableMvtLayer(
         { selectionOptions: { selectedFeatureIds } },
         deckProps,

frontend/src/data-layers/risks/risk-view-layer.ts
@@ -1,5 +1,4 @@
 import { createElement } from 'react';
-import GL from '@luma.gl/constants';
 import { RiskParams } from './domains';
 
 import { rasterTileLayer } from 'lib/deck/layers/raster-tile-layer';
@@ -58,7 +57,7 @@ export function riskViewLayer(riskType: string, riskParams: RiskParams): ViewLay
       return rasterTileLayer(
         {
           textureParameters: {
-            [GL.TEXTURE_MAG_FILTER]: GL.LINEAR,
+            magFilter: 'linear',
           },
           opacity: riskType === 'cyclone' ? 0.6 : 1,
         },

commit 58c741622dc60afdfc530b9060641e02962ffc40

fix(frontend): get feature IDs for adaptation maps
Fix feature IDs for data loader layers, which were broken in #266.

frontend/src/lib/deck/layers/data-loader-layer.ts
@@ -13,7 +13,8 @@ export function dataLoaderLayer(tileProps, { dataLoader }: DataLoaderOptions) {
     tile: { content },
   } = tileProps;
   if (content && dataLoader) {
-    const ids: number[] = !dataLoader.hasData ? content.map(getFeatureId) : [];
+    const ids: number[] = !dataLoader.hasData ? content.map((f) => getFeatureId(f, 'id')) : [];
+    console.log('dataLoaderLayer', ids);
 
     dataLoader.loadDataForIds(ids);
   }