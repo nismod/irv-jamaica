fa09a3f1415af837a032db2a4d7a5451a00412e6
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2025-03-18 16:42:49 +0000

    refactor(frontend): use react-query keys in fetcher functions
    Refactor `react-query` fetcher functions to take `{ queryKey }` as an argument.

--- frontend/src/lib/map/legend/use-color-map-values.ts
+++ frontend/src/lib/map/legend/use-color-map-values.ts
@@ -1,8 +1,10 @@
 import { useMemo } from 'react';
 import { useQuery } from '@tanstack/react-query';
 
-async function fetchColorMapValues(colorScheme: string) {
-  const response = await fetch(`/raster/colormap?colormap=${colorScheme}&stretch_range=[0,1]`);
+async function fetchColorMapValues({ queryKey }) {
+  const [endpoint, query] = queryKey;
+  const searchParams = new URLSearchParams(query);
+  const response = await fetch(`${endpoint}?${searchParams}`);
   if (!response.ok) {
     throw new Error('Failed to fetch color map values');
   }
@@ -11,14 +13,18 @@ async function fetchColorMapValues(colorScheme: string) {
 
 export function useRasterColorMapValues(colorScheme: string, stretchRange: [number, number]) {
   const [rangeMin, rangeMax] = stretchRange;
+  const query = {
+    colormap: colorScheme,
+    stretch_range: '[0,1]',
+  }
 
   const {
     isLoading,
     error,
     data = {},
   } = useQuery({
-    queryKey: ['colormaps', colorScheme],
-    queryFn: () => fetchColorMapValues(colorScheme),
+    queryKey: ['/raster/colormap', query],
+    queryFn: fetchColorMapValues,
   });
   const { colormap = [] } = data;
 
