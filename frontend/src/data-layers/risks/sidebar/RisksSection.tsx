b41080dca52d6ce2da8c3ac77f4ebbbf7754d6cc
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2025-05-22 16:45:49 +0100

    fix(frontend): hotspot storybook state
    Add `useSyncConfigState` to the hotspots sidebar section. Fixes a bug where the hotspot menus are empty in the storybook.

--- frontend/src/data-layers/risks/sidebar/RisksSection.tsx
+++ frontend/src/data-layers/risks/sidebar/RisksSection.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
 
+import { useSyncConfigState } from 'app/state/data-params';
+
 import { SidebarPanel } from 'lib/sidebar/SidebarPanel';
 import { SidebarPanelSection } from 'lib/sidebar/ui/SidebarPanelSection';
 import { ErrorBoundary } from 'lib/react/ErrorBoundary';
@@ -7,6 +9,7 @@ import { ErrorBoundary } from 'lib/react/ErrorBoundary';
 import { RisksControl } from './RisksControl';
 
 export const RisksSection: FC = () => {
+  useSyncConfigState();
   return (
     <SidebarPanel id="risks" title="Hotspots">
       <ErrorBoundary message="There was a problem displaying this section.">
