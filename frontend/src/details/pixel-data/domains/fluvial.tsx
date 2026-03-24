bdb876507d58a656a35b89915daf16b27e522166
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2026-03-24 10:27:53 +0000

    refactor: Pixel Driller data grid component (#422)
    Refactor the Pixel Driller data display into a new shared component for data grids.

--- frontend/src/details/pixel-data/domains/fluvial.tsx
+++ frontend/src/details/pixel-data/domains/fluvial.tsx
@@ -1,53 +1,29 @@
-import { DataGrid } from '@mui/x-data-grid';
 import { useAtomValue } from 'jotai';
 
-import {
-  pixelDrillerDataHeaders,
-  pixelDrillerDataRows,
-  pixelDrillerDataRPs,
-} from 'lib/state/pixel-driller';
+import { pixelDrillerDataRows } from 'lib/state/pixel-driller';
 
-import '../hazard-table.css';
 import { HazardAccordion } from '../hazard-accordion';
+import { PixelDataGrid } from '../pixel-data-grid';
 
 const title = 'River flooding';
 
-const displayReturnPeriods = new Set([5, 10, 20, 50, 100, 200, 500]);
-
-const PixelDataGrid = ({ pixel_layer }) => {
-  const headers = useAtomValue(pixelDrillerDataHeaders);
+const DataSection = ({ pixel_layer }) => {
   const rows = useAtomValue(pixelDrillerDataRows(pixel_layer));
-  const dataReturnPeriods = useAtomValue(pixelDrillerDataRPs(pixel_layer));
-  const columns = [
-    { field: 'epoch', headerName: 'Epoch', width: 55 },
-    { field: 'rcp', headerName: 'RCP', width: 55 },
-  ];
 
-  // Include confidence column if there are multiple values
-  const confidences = new Set(rows.map((d) => d.confidence));
-  if (confidences.size > 1) {
-    columns.push({ field: 'confidence', headerName: 'Confidence', width: 60 });
-  }
-
-  const returnPeriods = displayReturnPeriods.intersection(dataReturnPeriods);
-
-  returnPeriods.forEach((rp) => {
-    columns.push({ field: `rp-${rp}`, headerName: `RP ${rp}`, width: 60 });
-  });
-
-  if (!headers.length || !rows.length) {
+  if (!rows.length) {
     return null;
   }
+
   const variable = rows[0].variable;
   const unit = rows[0].unit;
 
   return (
     <>
       <HazardAccordion id={pixel_layer} title={`${title}: ${variable} (${unit})`}>
-        <DataGrid columns={columns} rows={rows} rowHeight={30} density="compact" />
+        <PixelDataGrid pixel_layer={pixel_layer} />
       </HazardAccordion>
     </>
   );
 };
 
-export default PixelDataGrid;
+export default DataSection;
---
+++ frontend/src/details/pixel-data/pixel-data-grid.tsx
@@ -0,0 +1,40 @@
+import { DataGrid } from '@mui/x-data-grid/DataGrid';
+import { useAtomValue } from 'jotai';
+
+import {
+  pixelDrillerDataHeaders,
+  pixelDrillerDataRows,
+  pixelDrillerDataRPs,
+} from 'lib/state/pixel-driller';
+
+import './pixel-data-grid.css';
+
+const displayReturnPeriods = new Set([5, 10, 20, 50, 100, 200, 500]);
+
+export const PixelDataGrid = ({ pixel_layer }) => {
+  const headers = useAtomValue(pixelDrillerDataHeaders);
+  const rows = useAtomValue(pixelDrillerDataRows(pixel_layer));
+  const dataReturnPeriods = useAtomValue(pixelDrillerDataRPs(pixel_layer));
+  const columns = [
+    { field: 'epoch', headerName: 'Epoch', width: 55 },
+    { field: 'rcp', headerName: 'RCP', width: 55 },
+  ];
+
+  // Include confidence column if there are multiple values
+  const confidences = new Set(rows.map((d) => d.confidence));
+  if (confidences.size > 1) {
+    columns.push({ field: 'confidence', headerName: 'Confidence', width: 60 });
+  }
+
+  const returnPeriods = displayReturnPeriods.intersection(dataReturnPeriods);
+
+  returnPeriods.forEach((rp) => {
+    columns.push({ field: `rp-${rp}`, headerName: `RP ${rp}`, width: 60 });
+  });
+
+  if (!headers.length || !rows.length) {
+    return null;
+  }
+
+  return <DataGrid columns={columns} rows={rows} rowHeight={30} density="compact" />;
+};
